6ad8c4b649c5274d28a33bcd3957ef4f7a6c9fcd
Ilya <ilia.il.27@mail.ru>
2024-02-19 20:46:24 +0300

    add controllers

---
+++ PrintManagement.Api/Controllers/EmployeeController.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using PrintManagement.Api.Models.Responses;
+using PrintManagement.BusinessLayer.Services;
+using PrintManagement.BusinessLayer.Services.Interfaces;
+
+namespace PrintManagement.Api.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class EmployeeController : ControllerBase
+{
+    private readonly IEmployeeService _employeeService;
+    private readonly IMapper _mapper;
+
+    public EmployeeController(IEmployeeService employeeService, IMapper mapper)
+    {
+        _employeeService = employeeService;
+        _mapper = mapper;
+    }
+
+    [HttpGet("[action]")]
+    [ProducesResponseType(typeof(List<AllEmployeeResponse>), StatusCodes.Status200OK)]
+    public async Task<ActionResult<List<AllEmployeeResponse>>> GetAllEmployees()
+    {
+        var branches = await _employeeService.GetAllEmployees();
+        return Ok(_mapper.Map<List<AllEmployeeResponse>>(branches));
+    }
+}
