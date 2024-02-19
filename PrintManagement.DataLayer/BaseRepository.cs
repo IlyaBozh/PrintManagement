50cdda87ed4215d9f531d97f30abd1dbcfc6eb81
Ilya <ilia.il.27@mail.ru>
2024-02-19 18:23:09 +0300

    Add BaseRepository & StoredProcedures

---
+++ PrintManagement.DataLayer/BaseRepository.cs
@@ -0,0 +1,16 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+
+namespace PrintManagement.DataLayer;
+
+public class BaseRepository
+{
+    private readonly IDbConnection _connection;
+
+    public BaseRepository(IDbConnection connection)
+    {
+        _connection = connection;
+    }
+
+    protected IDbConnection _connectionString => new SqlConnection(_connection.ConnectionString);
+}
---
+++ PrintManagement.DataLayer/StoredProcedures.cs
@@ -0,0 +1,9 @@
+﻿namespace PrintManagement.DataLayer;
+
+public class StoredProcedures
+{
+    public const string Branch_GetAll = "Branch_GetAll";
+    public const string Employee_GetAll = "Employee_GetAll";
+    public const string Printer_GetAll = "Printer_GetAll";
+    public const string Printer_GetByConnectionType = "Printer_GetByConnectionType";
+}
